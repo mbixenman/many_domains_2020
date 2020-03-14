f919d035a23ddf3240cb7ff1b9aa9b0b8957e1e9
mbixenman <m.bixenman1@gmail.com>
2020-03-14 19:20:23 -0400

    bug fixes applied

--- scrape_many_domains.py
+++ scrape_many_domains.py
@@ -20,19 +20,19 @@ for link in df['domain_name']:
 		print("Downloading (first attempt): ", link)
 		headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
 		fld_link = get_fld('http://' + link)
-		request_link - "http:www." + fld_link
+		request_link = "http:www." + fld_link
 		req = Request(url=link, headers=headers)
-		response - urlopen(request_link)
+		response = urlopen(req)
 		html = response.read()
 		f.write(html)
 		f.close()
 	except:
-		print("Downloading (second attepmt): ", link)
+		print("Downloading (second attempt): ", link)
 		headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
 		fld_link = get_fld('http://' + link)
 		request_link = "http://" + fld_link
 		req = Request(url=link, headers=headers)
-		response - urlopen(request_link)
+		response - urlopen(req)
 		html = response.read()
 		f.write(html)
 		f.close()
